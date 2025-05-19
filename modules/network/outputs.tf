776cd3d4ddc2e0ae3f4f54fc744f072cea791a9e
Tanabe Ken-ichi <kenichi.tanabe@hennge.com>
2025-05-19 14:26:45 +0900

    fix(network): expose more EIP attributes

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -2,3 +2,13 @@ output "network_interface_id" {
   description = "ENI ID"
   value       = aws_network_interface.tailscale.id
 }
+
+output "eip_allocation_id" {
+  description = "EIP Allocation ID"
+  value       = aws_eip.tailscale.allocation_id
+}
+
+output "eip_public_ip" {
+  description = "EIP Public IP"
+  value       = aws_eip.tailscale.public_ip
+}
